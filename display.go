7ca2a214538173590163c9c3ec607f698180c29f
DeedleFake <deedlefake@users.noreply.github.com>
2018-08-19 19:02:01 -0400

    Some refactoring and a bunch of Godoc comments.

--- display.go
+++ display.go
@@ -7,11 +7,16 @@ import (
 	"syscall/js"
 )
 
+// Display is a minimally featured wrapper around a canvas DOM element
+// instance.
 type Display struct {
 	canvas js.Value
 	ctx    js.Value
 }
 
+// NewDisplay returns a new Display instance that wraps the given
+// canvas element. It does not check to make sure that it has actually
+// been passed a canvas.
 func NewDisplay(canvas js.Value) *Display {
 	return &Display{
 		canvas: canvas,
@@ -19,6 +24,7 @@ func NewDisplay(canvas js.Value) *Display {
 	}
 }
 
+// FillRect fills the rectangle r with the color c.
 func (d *Display) FillRect(c color.Color, r image.Rectangle) {
 	d.ctx.Set("fillStyle", htmlColor(c))
 	d.ctx.Call("fillRect", r.Min.X, r.Min.Y, r.Dx(), r.Dy())
--- world.go
+++ world.go
@@ -1,16 +1,27 @@
 package main
 
+// Cell stores coordinates for a cell in the world.
 type Cell struct {
 	X, Y int
 }
 
+// World stores the state of the world as a set of live cells. If a
+// cell is present in the map as a key, then it is considered to be
+// live.
+//
+// For both convenience and compatibility, the IsLive method should be
+// used instead of manually checking for presence.
 type World map[Cell]struct{}
 
-func (w World) Live(c Cell) bool {
+// IsLive returns true if the given cell is live.
+func (w World) IsLive(c Cell) bool {
 	_, ok := w[c]
 	return ok
 }
 
+// Neighbors returns the number of live neighbors of the given cell,
+// whether or not that cell is itself live. It will always return a
+// number in the range [0, 8].
 func (w World) Neighbors(c Cell) (n int) {
 	for y := c.Y - 1; y <= c.Y+1; y++ {
 		for x := c.X - 1; x <= c.X+1; x++ {
@@ -19,7 +30,7 @@ func (w World) Neighbors(c Cell) (n int) {
 				continue
 			}
 
-			if w.Live(cell) {
+			if w.IsLive(cell) {
 				n++
 			}
 		}
@@ -28,7 +39,20 @@ func (w World) Neighbors(c Cell) (n int) {
 	return n
 }
 
-func (w World) LiveState(c Cell) bool {
+// NextState returns the state that the given cell should have in the
+// next world state given both its state and the state of its
+// neighbors in the current world state.
+func (w World) NextState(c Cell) bool {
+	if w.IsLive(c) {
+		return w.nextStateLive(c)
+	}
+
+	return w.nextStateDead(c)
+}
+
+// nextStateLive returns the next state of the cell at c assuming that
+// the cell is currently live.
+func (w World) nextStateLive(c Cell) bool {
 	n := w.Neighbors(c)
 
 	if (n < 2) || (n > 3) {
@@ -38,7 +62,9 @@ func (w World) LiveState(c Cell) bool {
 	return true
 }
 
-func (w World) DeadState(c Cell) bool {
+// nextStateDead returns the next state of the cell at c assuming that
+// the cell is currently dead.
+func (w World) nextStateDead(c Cell) bool {
 	n := w.Neighbors(c)
 
 	if n == 3 {
@@ -48,10 +74,12 @@ func (w World) DeadState(c Cell) bool {
 	return false
 }
 
+// Next returns a new World instance containing the next state
+// following the state contained by w.
 func (w World) Next() World {
 	next := make(World, len(w))
 	for c := range w {
-		if w.LiveState(c) {
+		if w.nextStateLive(c) {
 			next[c] = struct{}{}
 		}
 
@@ -62,7 +90,7 @@ func (w World) Next() World {
 					continue
 				}
 
-				if !w.Live(cell) && w.DeadState(cell) {
+				if !w.IsLive(cell) && w.nextStateDead(cell) {
 					next[cell] = struct{}{}
 				}
 			}
